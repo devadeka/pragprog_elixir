fa8ae2338c3a2f55bfe8a557560fb139ade15276
Devatanu Deka <deva.deka@gmail.com>
2018-11-13 17:12:10 +1100

    ListsAndRecurssion-1

--- Ch7/sum.ex
+++ Ch7/sum.ex
@@ -1,13 +1,22 @@
-defmodule ListAccum do
-    def sum(list), do: _sum(list, 0)
-    # private methods
-    defp _sum([], total), do: total
-    defp _sum([ head | tail ], total), do: _sum(tail, head+total)
-end
+defmodule MyList do
+    defmodule Accum do
+        def sum(list), do: _sum(list, 0)
+        # private methods
+        defp _sum([], total), do: total
+        defp _sum([ head | tail ], total), do: _sum(tail, head+total)
+    end
 
-defmodule ListNoAccum do
-    def sum(list), do: _sum(list)
-    # private methods
-    defp _sum([]), do: 0
-    defp _sum([ head | tail ]), do: head + _sum(tail)
+    #Exercise: ListsAndRecursion-0
+    defmodule NoAccum do
+        def sum(list), do: _sum(list)
+        # private methods
+        defp _sum([]), do: 0
+        defp _sum([ head | tail ]), do: head + _sum(tail)
+    end
+
+    #Exercise: ListsAndRecursion-1
+    defmodule Map do
+        def mapsum([], _), do: 0
+        def mapsum([ head | tail ], func), do: func.(head) + mapsum(tail, func)
+    end
 end
